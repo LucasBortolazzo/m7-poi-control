a7357bfe9bfce1ac6fb7c325e684ee7c3fae381d
Lucas Bortolazzo <lucas.dolberth@yahoo.com>
2022-08-15 23:56:48 -0300

    Implemented initial template

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
-declare var google: any;
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-home',
@@ -8,17 +10,33 @@ declare var google: any;
     styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-    private map: any;
+    private map: google.maps.Map;
 
-    constructor() {}
+    public formFiltro: FormGroup;
+
+    filteredOptionsPoi: Observable<any[]>;
+    filteredOptionsPlaca: Observable<any[]>;
+
+    constructor(private _fb: FormBuilder, private _snackBar: MatSnackBar) {}
 
     ngOnInit(): void {
-        let startPoint = { lat: -25.43615638835874, lng: -49.2589101856207 };
-        this.map = new google.maps.Map(document.querySelector('#map'), {
-            center: startPoint,
-            zoom: 15,
-        });
+        this._criarFormFiltro();
 
-        console.log(this.map);
+        let startPoint = { lat: -25.43615638835874, lng: -49.2589101856207 };
+        this.map = new google.maps.Map(
+            document.querySelector('#map') as HTMLElement,
+            {
+                center: startPoint,
+                zoom: 15,
+            }
+        );
+    }
+
+    private _criarFormFiltro() {
+        this.formFiltro = this._fb.group({
+            pontoInteresse: [null],
+            placa: [null],
+            dataLeitura: [null],
+        });
     }
 }
