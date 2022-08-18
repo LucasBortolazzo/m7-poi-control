70687dcee482538c32efec3f9ad1adc4a1455f0e
Lucas Bortolazzo <lucas.dolberth@yahoo.com>
2022-08-17 23:27:49 -0300

    Configured mock API(JSON-SERVER)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
 export const environment = {
     production: false,
-    api: 'https://challenge-backend.mobi7.io',
+
+    // Remote API
+    // api: 'https://challenge-backend.mobi7.io',
+
+    // Mock API - Before run in CMD: npm run api-and-front-end
+    api: 'http://localhost:3000',
 };
